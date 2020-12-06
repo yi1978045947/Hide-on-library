c59b1f9915c4f72831fa4466f7359eade9a0f335
yi1978045947 <1978045947@qq.com>
2020-12-06 19:37:57 +0800

    理解unsigned char的用法

---
+++ test2.c
@@ -0,0 +1,20 @@
+//int main()
+//{
+//	char a[1000]; 
+//int i;
+//for (i = 0; i < 1000; i++)
+//{
+//	a[i] = -1 - i;
+//}
+//printf("%d", strlen(a));
+//return 0;
+//}
+unsigned int i = 0;
+int main()
+{
+	for (i = 0; i <= 255; i++)
+	{
+		printf("Hello\n");
+	}
+	return 0;
+}
