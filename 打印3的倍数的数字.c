5b2bbd830c6b9cb217bdf94504bcfea5380d9203
yi1978045947 <1978045947@qq.com>
2021-04-02 12:47:36 +0800

    打印3的倍数的数字

---
+++ 打印3的倍数的数字.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return 0;
+}
