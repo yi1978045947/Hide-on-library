f45c92c19fc29b27c898562fa29413851572275e
yi1978045947 <1978045947@qq.com>
2021-04-06 08:08:32 +0800

    打印冒泡排序

---
+++ 打印冒泡排序.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+void BubbleSort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j]>arr[j + 1])
+			{
+				int tmp = 0;
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 1, 3, 4, 5, 7, 6, 89, 0, 10, 23, 5 ,-200 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, sz);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
