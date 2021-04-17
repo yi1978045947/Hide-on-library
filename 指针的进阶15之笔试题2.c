69b909496f1b5c739d1fd82fc6fe53b754d1748b
yi1978045947 <1978045947@qq.com>
2021-04-17 16:55:44 +0800

    指针的进阶15之笔试题2

---
+++ 指针的进阶15之笔试题2.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+int main()
+{
+	int a[5][5];
+	int(*p)[4];
+	p = a;
+	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+	return 0;
+}
