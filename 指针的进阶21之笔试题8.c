05a60c995680a5095894239ba0aa004013cb8e99
yi1978045947 <1978045947@qq.com>
2021-04-18 09:52:07 +0800

    指针的进阶21之笔试题8

---
+++ 指针的进阶21之笔试题8.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+int main()
+{
+	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	char**cp[] = { c + 3, c + 2, c + 1, c };
+	char***cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *--*++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
