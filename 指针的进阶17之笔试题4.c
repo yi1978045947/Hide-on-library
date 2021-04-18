4b3f1454d2d9ee0dbc1cb8d88a40869c11884152
yi1978045947 <1978045947@qq.com>
2021-04-18 09:43:07 +0800

    指针的进阶17之笔试题4

---
+++ 指针的进阶17之笔试题4.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+//由于还没学习结构体，这里告知结构体的大小是20个字节
+struct Test
+{
+	int Num;
+	char *pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p;
+//假设p 的值为0x100000。 如下表表达式的值分别为多少？
+int main()
+{
+	printf("%p\n", p + 0x1);
+	printf("%p\n", (unsigned long)p + 0x1);
+	printf("%p\n", (unsigned int*)p + 0x1);
+	return 0;
+}
