bc494430144e698eda62c9a772855186128b376d
yi1978045947 <1978045947@qq.com>
2021-04-18 09:44:17 +0800

    指针的进阶19之笔试题6

---
+++ 指针的进阶19之笔试题6.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include <stdio.h>
+int main()
+{
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int *ptr1 = (int *)(&aa + 1);
+	int *ptr2 = (int *)(*(aa + 1));
+	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+	printf("\n");
+	return 0;
+}
