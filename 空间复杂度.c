542616e106f14a104abf9109f7dc0d3cbf21e796
yi1978045947 <1978045947@qq.com>
2021-04-30 12:29:47 +0800

    空间复杂度

---
+++ 空间复杂度.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+// 计算BubbleSort的空间复杂度？
+void BubbleSort(int* a, int n)
+{
+	assert(a);
+	for (size_t end = n; end > 0; --end)
+	{
+		int exchange = 0;
+		for (size_t i = 1; i < end; ++i)
+		{
+			if (a[i - 1] > a[i])
+			{
+				Swap(&a[i - 1], &a[i]);
+				exchange = 1;
+			}
+		}
+		if (exchange == 0)
+			break;
+	}
+}
+int main()
+{
+	return 0;
+}
