8eaca2c7d856fb6cb9274d2f9e64360a0d9827d8
yi1978045947 <1978045947@qq.com>
2021-05-04 09:01:53 +0800

    LeetCode题目五十八之二叉树的翻转

---
+++ LeetCode题目五十八之二叉树的翻转.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+struct TreeNode
+{
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+struct TreeNode* invertTree(struct TreeNode* root)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	struct TreeNode* left = invertTree(root->left);
+	struct TreeNode* right = invertTree(root->right);
+	root->left = right;
+	root->right = left;
+	return root;
+}
+int main()
+{
+	return 0;
+}
