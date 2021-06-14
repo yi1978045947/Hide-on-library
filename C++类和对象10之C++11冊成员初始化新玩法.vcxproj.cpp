379915ef98c5a9e346c2cc9b7e22ad81573737a3
yi1978045947 <1978045947@qq.com>
2021-06-14 22:14:30 +0800

    C++类和对象10之C++11冊成员初始化新玩法.vcxproj

---
+++ C++类和对象10之C++11冊成员初始化新玩法.vcxproj.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<iostream>
+using namespace std;
+class B
+{
+public:
+	B(int b=0 )
+		:_b(b)
+	{}
+	int _b;
+};
+class A
+{
+public:
+	void Print()
+	{
+		cout << a << endl;
+		cout << b._b << endl;
+		cout << p << endl;
+	}
+private:
+	// 非静态成员变量，可以在成员声明时给缺省值。
+	int a = 10;
+	B b=10 ;
+	int* p = (int*)malloc(4);
+	static int n;
+};
+int A::n = 10;
+int main()
+{
+	A a;
+	a.Print();
+	return 0;
+}
