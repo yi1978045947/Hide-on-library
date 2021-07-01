00a03e34969c5cb35201548e63b5ca03dbb56618
yi1978045947 <1978045947@qq.com>
2021-07-01 17:53:30 +0800

    system共享内存

---
+++ 共享内存/head.h
@@ -0,0 +1,5 @@
+#pragma once 
+#define PATHNAME "/tmp"
+#define PROJ_ID 0x6688
+
+#define SIZE 4096
---
+++ 共享内存/server.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include"head.h"
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+int main()
+{
+    key_t k=ftok(PATHNAME,PROJ_ID);
+    printf("key=%d\n",k);
+    int shmid=shmget(k,SIZE,IPC_CREAT|IPC_EXCL|0666);//创建共享内存
+
+    if(shmid==-1)
+    {
+        printf("创建共享内存失败!\n");
+        return 1;
+    }
+    printf("shmid=%d\n",shmid);
+    sleep(10);
+
+    char* str=(char*)shmat(shmid,NULL,0);//关联共享内存
+   // sleep(5);
+   while(1)
+   {
+       sleep(1);
+       printf("%s\n",str);
+   }
+
+    shmdt(str);//去除关联的共享内存
+   // sleep(5);
+
+    shmctl(shmid,IPC_RMID,NULL);//删除共享内存
+    sleep(5);
+    return 0;
+}
