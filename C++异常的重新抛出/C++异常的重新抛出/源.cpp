8c0d5c9b754d2277c35f5a3a583ce5530ff6a15e
yi1978045947 <1978045947@qq.com>
2021-09-01 22:57:22 +0800

    C++异常的重新抛出

---
+++ C++异常的重新抛出/C++异常的重新抛出/源.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<iostream>
+using namespace std;
+
+
+double Division(int a, int b)
+{
+	// 当b == 0时抛出异常
+	if (b == 0)
+	{
+		throw "Division by zero condition!";
+	}
+	return (double)a / (double)b;
+}
+void Func()
+{
+	// 这里可以看到如果发生除0错误抛出异常，另外下面的array没有得到释放。
+	// 所以这里捕获异常后并不处理异常，异常还是交给外面处理，这里捕获了再
+	// 重新抛出去。
+	int* array = new int[10];
+	try {
+		int len, time;
+		cin >> len >> time;
+		cout << Division(len, time) << endl;
+	}
+	catch (...)
+	{
+		cout << "delete []" << array << endl;
+		delete[] array;
+		throw;
+	}
+	// ...
+	cout << "delete []" << array << endl;
+	delete[] array;
+}
+int main()
+{
+	try
+	{
+		Func();
+	}
+	catch (const char* errmsg)
+	{
+		cout << errmsg << endl;
+	}
+	return 0;
+}
