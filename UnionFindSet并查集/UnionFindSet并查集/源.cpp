b142856ca642ae501dd01a07d18910c70c466b40
yi1978045947 <1978045947@qq.com>
2021-10-06 11:10:33 +0800

    UnionFindSet并查集

---
+++ UnionFindSet并查集/UnionFindSet并查集/源.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+#include<iostream>
+#include<vector>
+#include<assert.h>
+using namespace std;
+
+class UnionFindSet
+{
+public:
+	UnionFindSet(size_t n)
+	{
+		_ufs.resize(n, -1);
+	}
+	
+	int FindRoot(int x)
+	{
+		assert(x < _ufs.size());
+		while (_ufs[x] >= 0)
+		{
+			x = _ufs[x];
+		}
+		return x;
+	}
+	
+	void Union(int x1, int x2)
+	{
+		assert(x1 < _ufs.size());
+		assert(x2 < _ufs.size());
+
+		int root1 = FindRoot(x1);
+		int root2 = FindRoot(x2);
+		if (root1 == root2)
+		{
+			return;
+		}
+		if (root1 != root2)
+		{
+			_ufs[root1] += _ufs[root2];
+			_ufs[root2] = root1;
+		}
+	}
+
+	//负数的个数就是集合的个数
+	size_t SetSize()
+	{
+		size_t count = 0;
+		for (size_t i = 0; i < _ufs.size(); i++)
+		{ 
+			if (_ufs[i] < 0)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+private:
+	vector<int> _ufs;
+};
